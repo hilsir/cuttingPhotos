6c8a3a8b3120c96fd21a3651a47c77645f12c488
hilsir <romich-r@bk.ru>
2026-05-05 13:55:59 +0800

    start

---
+++ cutting.py
@@ -0,0 +1,38 @@
+import cv2, os
+from pathlib import Path
+from ultralytics import YOLO
+from datetime import datetime
+from dotenv import load_dotenv
+load_dotenv()
+
+# Планируется, что эта система будет работать на файловом сервере
+# Поэтому должно работать на cpu
+
+class ImageCutter:
+    def __init__(self):
+        model_path = os.getenv("MODEL_PATH")
+        self.model = YOLO(model_path)
+
+    def process_image(self, image_path: str, output_path: str):
+        img = cv2.imread(image_path)
+
+        if img is None:
+            print(f"Ошибка: Не удалось загрузить {image_path}")
+            return
+
+        detected_goods = self.model(img)
+
+        # Нарезка найденных объектов
+        for detected_good in detected_goods:
+            for box in detected_good.boxes:
+                # Координаты на товара фотографии
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                # Обрезаем изображение
+                cutting_img = img[y1:y2, x1:x2]
+
+                # Формируем имя файла по времени
+                times = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
+                cutting_img_name = f"{output_path}/img_{times}.png"
+
+                cv2.imwrite(str(cutting_img_name), cutting_img)
+
---
+++ main.py
@@ -0,0 +1,54 @@
+
+import os
+from pathlib import Path
+import time
+from datetime import datetime, timedelta, timezone
+from cutting  import ImageCutter
+from dotenv import load_dotenv
+load_dotenv()
+
+def start ():
+    run_time_str = os.getenv("RUN_TIME")
+    irkutsk_tz = timezone(timedelta(hours=8))
+
+    # Бог машина не кокорай меня за такую ересь
+    while True:
+        current_time = datetime.now(irkutsk_tz).strftime("%H:%M")
+        if current_time == run_time_str:
+            processing()
+            # Чтобы не зайти в это же условие
+            time.sleep(61)
+
+        time.sleep(30)
+
+def processing():
+    img_paths = os.getenv("IMG_PATHS")
+    output_dir = os.getenv("OUTPUT_DIR")
+    list_img_paths = [p.strip() for p in img_paths.split(",") if p.strip()]
+
+    # Загружаем модель
+    cutter = ImageCutter()
+
+    for path_str in list_img_paths:
+
+        input_path_obj = Path(path_str)
+        input_folder_name = input_path_obj.name
+        output_path = f"{output_dir}/{input_folder_name}"
+        output_path_obj = Path(output_path)
+
+        if not output_path_obj.exists():
+            output_path_obj.mkdir(parents=True, exist_ok=True)
+            print(f"Создана отсутствующая папка: {output_path}")
+
+        # Получаем файл с присмтавкой _final
+        file_path = next(input_path_obj.glob("*_final.*"), None)
+
+        if not file_path:
+            print(f"В папке {input_path_obj} нет файлов с пометкой '_final'")
+            return
+
+        cutter.process_image(file_path, output_path)
+
+if __name__ == "__main__":
+    print("start")
+    start()
